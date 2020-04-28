2c5af47b99e13f3f8a8f9e25f48174238da0f203
yusaku0515 <y.iwata0515@gmail.com>
2020-04-29 02:15:32 +0900

    5-9[finish3]

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,6 @@ class Book < ApplicationRecord
 	validates :user_id, presence: true
 	validates :title, presence: true
     validates :body, presence: true, length: { maximum: 200 }
-    validates :body, presence: true
 
 	# def user
 	# 	return User.find_by(id: self.user_id)
