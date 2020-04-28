034dca10080270e897346e0e499bd9396356fa0e
yusaku0515 <y.iwata0515@gmail.com>
2020-04-28 18:04:05 +0900

    5-9[finish2]

--- db/migrate/20200426125350_userimage.rb
+++
@@ -1,5 +0,0 @@
-class Userimage < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :user_images
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_125350) do
+ActiveRecord::Schema.define(version: 2020_04_26_124531) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
