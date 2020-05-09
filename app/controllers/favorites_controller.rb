50311ba76e3f0b70adb3a3b63e4aff178ded8eef
yusaku0515 <y.iwata0515@gmail.com>
2020-05-10 00:29:22 +0900

    favorite[非同期通信]start

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,12 @@
 class FavoritesController < ApplicationController
 
+
+
 	def create
 		books = Book.find(params[:book_id])
 		favorite = current_user.favorites.new(book_id: books.id)
 		favorite.save
-		redirect_back(fallback_location: root_path)
+
 	end
 
 
@@ -12,7 +14,8 @@ class FavoritesController < ApplicationController
         books = Book.find(params[:book_id])
         favorite = current_user.favorites.find_by(book_id: books.id)
         favorite.destroy
-        redirect_back(fallback_location: root_path)
+
 	end
+
 
 end
